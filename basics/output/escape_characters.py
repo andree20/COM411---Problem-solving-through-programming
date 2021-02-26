69fd14238aeb638b99899d9f592f0844604fe252
repl.it user <replituser@example.com>
2021-02-26 11:27:40 +0000

    escape characters added

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#displaying escape characters
+
+print("\t displays a tab space")
+print("\\ displays a backslash")
+print("\" displays double quote")
+print("\" displays single quote")
---
+++ basics/output/multiline_mesage.py
@@ -0,0 +1,7 @@
+#
+print("system failure imminent!")
+print("system reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("rebooting output motors")
+print("rebooting hover engine\n")
+print("system online")
